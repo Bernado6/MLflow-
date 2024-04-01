af343a4730d2c36af176ec09b4774aaf62a9692e
Bernardo6 <bernardo98@students.uonbi.ac,ke>
2024-04-01 17:53:43 +0300

    Added Model versioning & saving Model Artifacts

--- basic_ml.py
+++ basic_ml.py
@@ -7,6 +7,16 @@ from sklearn.linear_model import ElasticNet
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_absolute_error, r2_score, accuracy_score, mean_squared_error
+import uuid
+from datetime import datetime, timedelta
+
+
+time_day = datetime.utcnow() + timedelta(hours=3)
+formatted_time_day  = time_day.strftime('%Y-%m-%d-%H-%M')
+unique_id = str(uuid.uuid4().hex)[:6]
+unique_format = formatted_time_day + '-' + unique_id
+run_name_model_version = "trained-V" + unique_format
+print("Run name: ", run_name_model_version)
 
 
 
@@ -19,7 +29,8 @@ def load_data():
        raise e
   
 def eval(actual, pred) :
-    rmse = mean_squared_error(actual, pred, squared=False)
+    # rmse = mean_squared_error(actual, pred, squared=False)
+    rmse = mean_squared_error(actual, pred)
     mae = mean_absolute_error(actual, pred)
     r2 =  r2_score(actual, pred)
       
@@ -35,8 +46,8 @@ def main(alpha, l1_ratio):
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=6, test_size=0.2)
     mlflow.set_tracking_uri("http://localhost:5000")
-    mlflow.set_experiment("ML-Model-WineQuality-1")
-    with mlflow.start_run():
+    mlflow.set_experiment("Red-Wine-Model")
+    with mlflow.start_run(run_name = run_name_model_version):
         mlflow.log_param("Alpha", alpha)
         mlflow.log_param("l1-ratio", l1_ratio)
         
@@ -55,14 +66,18 @@ def main(alpha, l1_ratio):
         with open("dummy/example.txt", "wt") as f:
             f.write(f"Artifact created at {time.asctime()}")
         mlflow.log_artifacts("dummy")
+        mlflow.sklearn.save_model(model,
+                                  os.path.join("Model" ,
+                                               "Artifactcs",
+                                               unique_format))# Change my_model to path of your choice to save the model
         
         
         
         
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--alpha", "-a", type=int, default=0.2)
-    args.add_argument("--l1_ratio", "-l1", type=int, default=0.3)
+    args.add_argument("--alpha", "-a", type=float, default=0.2)
+    args.add_argument("--l1-ratio", "-l1", type=float, default=0.3)
     parser_args = args.parse_args()
     
     main(parser_args.alpha, parser_args.l1_ratio)
